fe5c30c4836a62a8c245b20dbf303539c801ab86
jakpren <praveen.r@continube.com>
2024-11-19 18:31:32 +0530

    type change

--- src/CodeEditorToTOML.js
+++ src/CodeEditorToTOML.js
@@ -3,15 +3,17 @@ import MonacoEditor from '@monaco-editor/react';
 
 
 const CodeEditorToTOML = () => {
-  const [code, setCode] = useState(`console.log("Hello world!");`);
+  const [code, setCode] = useState(`[owner]
+name = "Tom Preston-Werner"
+dob = 1979-05-27T07:32:00Z`);
 
   // Function to download the content as a .toml file
   const handleDownloadTOML = () => {
-    const blob = new Blob([code], { type: 'text/javascript' });
+    const blob = new Blob([code], { type: 'text/toml' });
     const url = URL.createObjectURL(blob);
     const a = document.createElement('a');
     a.href = url;
-    a.download = 'content.js';  // Set the filename for the download
+    a.download = 'content.toml';  // Set the filename for the download
     a.click();
     URL.revokeObjectURL(url);  // Clean up the URL object
   };
@@ -21,7 +23,7 @@ const CodeEditorToTOML = () => {
     <h4>Code Editor</h4>
       <MonacoEditor
         height="500px"
-        language="javascript"  // Set the language to TOML for syntax highlighting
+        language="toml"  // Set the language to TOML for syntax highlighting
         theme="vs-dark" 
         value={code}
         onChange={(newValue) => setCode(newValue)}  // Update the code state on change
